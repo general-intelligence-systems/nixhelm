5fe567cccc992cf8c2e7a6a70fc511686825fb12
Nathan Kidd <nathankidd@hey.com>
2026-03-13 16:32:37 +0000

    Quick Git Push

--- flake.nix
+++ flake.nix
@@ -45,13 +45,6 @@
       {
         charts = mapCharts;
 
-        packages.ci-image = pkgs.dockerTools.buildLayeredImage (import ./image.nix {
-          inherit pkgs;
-          inherit (pkgs) lib;
-          org = "general-intelligence-systems";
-          registry = "ghcr.io/general-intelligence-systems";
-        });
-
         formatter = pkgs.nixfmt-tree;
 
         devShell = pkgs.mkShell {
