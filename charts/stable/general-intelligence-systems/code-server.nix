51a5b6f6048e2d095a08d6b907149c38d68f6a89
Nathan Kidd <nathankidd@hey.com>
2026-03-14 04:37:20 +0000

    added code-server

---
+++ charts/stable/general-intelligence-systems/code-server.nix
@@ -0,0 +1,8 @@
+{
+  repo = "https://general-intelligence-systems.github.io/code-server-helm";
+  chart = "code-server";
+  latest = "3.33.0";
+  versions = {
+    "3.33.0" = "sha256-jg/ImdUFwKn4R/KLE16hzOKjrGb1O3Hq1DvoFyT1e7k=";
+  };
+}
