124d76fcb4f72f69e2fd8e99b52ad4acc3f4441a
Nathan Kidd <4201500+n-at-han-k@users.noreply.github.com>
2026-03-14 14:54:24 +0000

    Delete charts/stable/bitnami directory

--- charts/stable/bitnami/dataplatform-bp2.nix
+++
@@ -1,8 +0,0 @@
-{
-  repo = "https://charts.bitnami.com/bitnami";
-  chart = "dataplatform-bp2";
-  latest = "12.0.5";
-  versions = {
-    "12.0.5" = "sha256-m+z/ShvRPqp4+/Zj2qonlKtSYHYzA8VsPTyehQmzSZ8=";
-  };
-}
