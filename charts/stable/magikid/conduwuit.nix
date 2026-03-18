95d6a5b6b40cfd28246b5382006092f933ba2763
Nathan Kidd <nathankidd@hey.com>
2026-03-18 03:54:28 +0000

    added matrix

---
+++ charts/stable/magikid/conduwuit.nix
@@ -0,0 +1,13 @@
+{
+  repo = "oci://ghcr.io/magikid/modern-conduwuit-helm";
+  chart = "conduwuit";
+  latest = "2.1.0";
+  versions = {
+    "2.1.0" = "sha256-L5+tC24XMn2ylBiMpXP70OaDY/GduBgqhnPumcwtv48=";
+    "2.0.1" = "sha256-fBI1m3yiopr1GwRPanZ/CUGm/V3c7Vj4+HsyEqOPmdg=";
+    "2.0.0" = "sha256-PXOOO9wO9YowGDj/Ak0SfEI9SVepPPTF5bxfIUsH8rU=";
+    "1.0.2" = "sha256-Q7ylevA+hxDILUh6Fy2Wp+2CRpUBgsmbmGIzg+6jvvQ=";
+    "1.0.1" = "sha256-x22LWqat0PBqdwIXzJbIuFW/sMx/J1C81s0ri9R3kpE=";
+    "1.0.0" = "sha256-4PaNeh+lqRjN9wrYRHNcmb+UsutzTvchP1/ygnLm/64=";
+  };
+}
