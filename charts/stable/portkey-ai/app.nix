2d4101d72666e830819dde8cbe0d0389d79853fa
Nathan K <nathankidd@hey.com>
2026-04-07 23:17:13 +0000

    charts: add portkey-ai (gateway, app) as stable

---
+++ charts/stable/portkey-ai/app.nix
@@ -0,0 +1,29 @@
+{
+  repo = "https://portkey-ai.github.io/helm";
+  chart = "app";
+  latest = "1.7.0";
+  versions = {
+    "1.7.0" = "sha256-qCwTqN24gc2X/aX5mXgdvfeK6oQfVqiRTsdeQTURJcQ=";
+    "1.6.0" = "sha256-OqKXTSbNVuQ36/R63cOoKAhYmw5RtLs9syt3gahz1jE=";
+    "1.5.1" = "sha256-p0Yy2TcsojZUo1wGcKnFZtx6MbYhYLGYYfdlDSu7K9Q=";
+    "1.5.0" = "sha256-i6hx+XNQ2CIFkzflUaIeOZLA7tcBDVPk8ilgQlB1SJ0=";
+    "1.4.3" = "sha256-QmI/QwSOtZPRW+l8N0lD8vHL4iKPMnFIMFzTHPnB65I=";
+    "1.4.2" = "sha256-6SD/4DBHff+PHuAKgDUyWomJYJ1qNgHz58AdIhMNG0k=";
+    "1.4.1" = "sha256-uWq8eKYfoaqR6WFsYzWuRMG3pSnmhDMjn0h5U9B6dK0=";
+    "1.4.0" = "sha256-1joL6nY1glijaxQkyw3QhF6ffzX54VDURNEZi1QDUiw=";
+    "1.3.0" = "sha256-wJ2p/LIaTcX8AQiVoRNN/bKWc6/V9C9bvwpLEWQgDUE=";
+    "1.2.2" = "sha256-Vi+cmLYOLfBtn3bh52Ln36HY4y/G750sgsVRatu7vE4=";
+    "1.2.1" = "sha256-qDcci5ReBG1jXxDh9Rq18jyfd/cQSs+g1chbaORkQss=";
+    "1.2.0" = "sha256-as/IzqVuVvbxcCeACzADrk7S/clCkve/o+mForEavEs=";
+    "1.1.0" = "sha256-msqQAB1ZfrHmXWxulP5Zqjj3lvi8QYuczp3TBy1uIUM=";
+    "1.0.13" = "sha256-tBktxAEaqRp216rgt2FoC5ZiBo4aACl3A+HvtlMPUXo=";
+    "1.0.12" = "sha256-d9R+XbBbWILJjMwWruy5iQzHifpUElmYesIIN6oUDoM=";
+    "1.0.11" = "sha256-USPLuBOrPYoNLWOo5HZPUQUJJkr4c3iuva0q3eV4RoQ=";
+    "1.0.10" = "sha256-G5ushiPLMV6XpvdcI5fLPOug2xIXCNEPQTTK+AouEFk=";
+    "1.0.9" = "sha256-HpESZd8EmK43e1lghcR+DPqxx0vzMmvofXrnNaXW1Kg=";
+    "1.0.8" = "sha256-CQsFobmwZSNn3CxdvpIsdR2q8B2ErAKANDS1grBu5Wg=";
+    "1.0.7" = "sha256-a8DVcwEORc0NWxvoiEmd8leDg3S9JlC74ay8Txn5wyA=";
+    "1.0.6" = "sha256-JEpE2IvsxYVIqSCshEp3eeQXmL41XzJ+EU4sZ5AbH10=";
+    "1.0.4" = "sha256-G6ykPVM9wdNmQdi/Oejl29I1cf0NiRI3L2HhelEolFc=";
+  };
+}
---
+++ charts/stable/portkey-ai/gateway.nix
@@ -0,0 +1,35 @@
+{
+  repo = "https://portkey-ai.github.io/helm";
+  chart = "gateway";
+  latest = "1.7.0";
+  versions = {
+    "1.7.0" = "sha256-gxm8jLKMTXedp57ew11xU0sPZw0/2NWYEi9WQ+y8Lmw=";
+    "1.6.4" = "sha256-ZwauB83EVY1y3A6XAd7CvfRhk9XaG4ZfbdRY2tNn4I8=";
+    "1.6.3" = "sha256-dsNuBwhYc3oNB6hGG2KjXYA2Lq/1swQ6aSZNezxRo8E=";
+    "1.6.2" = "sha256-TyB9/3PQSS5mQLjEicgjOx/oOtWumnCvU7Yjm7dsTCg=";
+    "1.6.1" = "sha256-yx96xTyEik89dNOA7yppS/uatHRzRFuPKTxyM8/4Isw=";
+    "1.6.0" = "sha256-jy0jhlrVH9BfgSzzqU6cxr80EFfGgWVvoam2sNX7d1w=";
+    "1.5.1" = "sha256-hh3WOu7dSa69fICifWT8qfZWYtdXzXmotMbtZMj0/+I=";
+    "1.5.0" = "sha256-4t7SmCZaGYdgnlbuBxG4/aWvSsUgMD6RbR/xoOGtUSc=";
+    "1.4.1" = "sha256-1XAWz3Zd4MCxUgiiG72tKB1aTIv1XaTE+jyfnnO10wQ=";
+    "1.4.0" = "sha256-CG44s4fIaw9Piq/NpgPQLMQRSS8mH9k9tlgbwVUE0jU=";
+    "1.3.3" = "sha256-BajH1BsglyX1bYvZ15t3JFtAoQVmIhyK6jbfbJMwNiE=";
+    "1.3.2" = "sha256-Iah5xEa52dZeraryffoEbrmdCk/aau3dP4uCnoMDl60=";
+    "1.3.1" = "sha256-rEce0dW0vqMIc2Xw9qbIQtYojKjFxan1dI1ogjCMqeM=";
+    "1.3.0" = "sha256-zp+jmFu3nQ6whKa0aKeFOuCda4TMLeN6vCFyh8f7bcI=";
+    "1.2.0" = "sha256-VBL8C1gsiQy1XLrOFl9J6ZgE9E/vSViBDM3wE3XdGb8=";
+    "1.1.7" = "sha256-UO6Dj5fd7Hay2S7DJ2fdQZO2nBfcn2KJRxP7hGtMlvo=";
+    "1.1.6" = "sha256-UzZFhO9t4m8gXG7sxLK4f1LCPzqb9OhcNGdJhm0+Qpc=";
+    "1.1.5" = "sha256-SaGfCKCzb5+AKezQ8jzbYYX70z/ylWYz+KLQSo/OG4g=";
+    "1.1.4" = "sha256-oSykwLLMH31EGGjWsnngnI9WQ+1fqTm2KuvrG6CCEo8=";
+    "1.1.3" = "sha256-j9WywmEF2B+fJInqnhhyPPq8KgH54vA/qjWGAPQf8nI=";
+    "1.1.2" = "sha256-qj2d2XorzrIzn1hidshXSSJ5Cr4/1z+g4/jw8zfryeI=";
+    "1.1.1" = "sha256-pIvgdVrCHLhBmZSMchPvaedG3hQpT3PP0c9G7lNjIS0=";
+    "1.1.0" = "sha256-Dtvh5wS/RBGO05zmWDKcP+MfurwYBT29fqMS7Ml6RhQ=";
+    "1.0.7" = "sha256-q7UHnq5Ge+Rv1h79QKh7TWe+uWz7LhoC3eX9w7RE4mE=";
+    "1.0.6" = "sha256-cnmzT6b6PNTeNJEgsTkRnzToHRzXvsZUMSHUoafOpCk=";
+    "1.0.5" = "sha256-ufYXZHJRRbcfA32kumSM8W9hu7W9wlLe8Tu6LvVhMJ4=";
+    "1.0.4" = "sha256-1SbSlNXrRSDh7bgGzNF3J9C08qqUWeqZqrT/nfncofg=";
+    "1.0.3" = "sha256-NljRke3Ts+ZBXA64MuSWPkPmTSP/+g3t9CzJImiTH6o=";
+  };
+}
