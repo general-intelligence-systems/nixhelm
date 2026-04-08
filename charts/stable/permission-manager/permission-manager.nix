3be448d2e831cba01f01aa5261a2d3703f2954a4
Nathan K <nathankidd@hey.com>
2026-04-08 18:40:15 +0000

    charts: add permission-manager helm chart

---
+++ charts/stable/permission-manager/permission-manager.nix
@@ -0,0 +1,9 @@
+{
+  repo = "https://sighupio.github.io/permission-manager";
+  chart = "permission-manager";
+  latest = "0.1.1";
+  versions = {
+    "0.1.1" = "sha256-TOsux8KOecZ1yYMYSfKCQ2Sg96S2jAbVkazrUfF+fYA=";
+    "0.1.0" = "sha256-viKQrcYvsEql2pY0NvcQXWa0kBv/KP7H2FgkEC56SDs=";
+  };
+}
