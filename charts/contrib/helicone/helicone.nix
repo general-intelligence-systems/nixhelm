2eed07c92f388505a58a27060f5bcbc9e0c2419e
Nathan K <nathankidd@hey.com>
2026-04-08 02:24:06 +0000

    Quick Git Push

---
+++ charts/contrib/helicone/helicone.nix
@@ -0,0 +1,8 @@
+{
+  repo = "https://helicone.github.io/HELM";
+  chart = "helicone";
+  latest = "0.1.42";
+  versions = {
+    "0.1.42" = "sha256-u3OJ0pI9+rWT/K8jMos+wH5wL3ZZdhV1o6mvaQKu2Xc=";
+  };
+}
